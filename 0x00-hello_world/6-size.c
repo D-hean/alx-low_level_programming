982de2cb4949ca7652741af632c51c3d1e514c0e
Sharif Moro <sharifmoro@yahoo.com>
2023-06-15 20:16:14 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@ int b;
 long int c;
 long long init d;
 float f;
-printf("size of a char:%lu/n", (unsigned long)sizeof(a));
-printf("size of a int:%lu/n", (unsigned long)sizeof(b));
-printf("size of a long int:%lu/n", (unsigned long)sizeof(c));
-printf("size of a long long int:%lu/n", (unsigned long)sizeof(d));
-printf("size of a float:%lu/n", (unsigned long)sizeof(f));
+printf("size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a int:%lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int:%lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int:%lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float:%lu bytes(s)\n", (unsigned long)sizeof(f));
 }
