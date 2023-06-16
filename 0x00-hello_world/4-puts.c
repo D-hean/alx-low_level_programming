044c4231129e655e9bced5f45e9973b4c46039aa
Sharif Moro <sharifmoro@yahoo.com>
2023-06-15 19:16:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using the puts function to print a line
+ * Return: 0
+ */
+init main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
