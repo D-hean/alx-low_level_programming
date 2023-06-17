749bdc843a3d01efc8e7ad18b5afb12741df85b3
Sharif Moro <sharifmoro@yahoo.com>
2023-06-17 05:13:11 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program which shows whether a last number is positive or negative
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int ld = n % 10;
+if (ld > 5)
+printf("The last digit of %d is %d and is greater than 5\n", n, ld);
+else if (ld == 0)
+printf("The last digit of %d is %d and is zeo\n", n, ld);
+else if (ld < 6 && ld != 0)
+printf("The last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+return (0);
+}
