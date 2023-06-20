e253b2e6471c3232b2b47e32da91efdf5a124651
Sharif Moro <sharifmoro@yahoo.com>
2023-06-20 12:29:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main - Program that prints _putchar followed by newline
+ * Return: 0 Always
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
