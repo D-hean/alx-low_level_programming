b0472aae10dca2959e7a6568f14cb0c65684e245
Sharif Moro <sharifmoro@yahoo.com>
2023-06-20 18:04:57 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of value
+ * Return: absolute value
+ * @n: the number evaluated
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else if (n < 0)
+	return (-n);
+}
