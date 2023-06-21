6aa92cf63a2139316e205fc8781ad0754f0e995f
Sharif Moro <sharifmoro@yahoo.com>
2023-06-20 17:04:03 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_islower - Checks if character is an alphabet
+* @c:The variable been checked
+* Return: 1 (Success)otherwise return 0
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')||(c >='A' && c <= 'Z')
+	return (1);
+else
+	return (0);
+}
