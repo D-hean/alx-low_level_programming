a8d097e9a56bbba09d1dcf082877d60887f899e8
Sharif Moro <sharifmoro@yahoo.com>
2023-06-20 19:59:33 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addition of integers
+ * @j: first number
+ * @k: second number
+ * Return: sum of numbers
+ */
+int add(int j,  int k)
+{
+int i;
+i = j + k;
+return (i);
+}
